e56a728f77ab05818a5cda51fbe7b6bf91b4f72b
Jaiden Khosla <74748528+JaidenKhosla@users.noreply.github.com>
2025-08-04 14:30:24 -0500

    Fix?

--- lib/database.ts
+++ lib/database.ts
@@ -5,7 +5,7 @@ import postgres from "postgres";
 import { createClient } from "@supabase/supabase-js";
 
 
-const URL_EXPIRATION = 60; //in seconds
+const URL_EXPIRATION = 60_000; //in milliseconds
 
 
 const fallbackImage = process.env.FALLBACK_IMAGE!;
@@ -31,7 +31,7 @@ export default async function serveFile(bucketName:string, filePath: string) {
             return req?.link!;
     }
 
-    const { data, error } = await supabase.storage.from(bucketName)?.createSignedUrl(filePath, URL_EXPIRATION);
+    const { data, error } = await supabase.storage.from(bucketName)?.createSignedUrl(filePath, URL_EXPIRATION/1000);
 
     if(error) return fallbackImage;
     
