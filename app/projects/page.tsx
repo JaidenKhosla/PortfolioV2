fd3b175739501841aa3252ef8f407b6e1197235e
Jaiden Khosla <74748528+JaidenKhosla@users.noreply.github.com>
2025-08-04 16:30:21 -0500

    Styling

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -7,9 +7,9 @@ export default async function Projects() {
 
     const projects = await fetchAllProjects();
     
-    return (<Fade>
+    return (<Fade className="flex flex-col items-center md:items-start">
         <h1>Projects</h1>
-        <p className="ml-0.5 max-w-2xl text-base text-white leading-relaxed">{"Over the years, I've worked on many tiny projects (most are just little games I made to cure boredom). All of them are open source and on Github so feel free to check out the code. All of them are deployed through Github Pages."}</p>
+        <p className="ml-0.5 max-w-2xl text-4xl md:text-xl text-white leading-relaxed">{"Over the years, I've worked on many tiny projects (most are just little games I made to cure boredom). All of them are open source and on Github so feel free to check out the code. All of them are deployed through Github Pages."}</p>
         <div  className="py-20 flex gap-6 flex-wrap pr-5 max-w-2xl`">
             {
              projects.map(async (project)=>{
--- ui/project/ProjectCard.tsx
+++ ui/project/ProjectCard.tsx
@@ -28,12 +28,12 @@ export default function ProjectCard({
     }, [])
 
 
-    return <>{(usePromise) && <Link className="w-100 select-none transition hover:scale-103 cursor-pointer group h-max" href={link} target="_blank">
-        <ImageSkeleton width={1600} height={900} alt={`An Image of ${title}`} draggable={false} className="w-full h-30 rounded-t-4xl object-cover transition group-hover:grayscale-90" promise={usePromise}/>
-        <div className="text-white bg-gray-700/60 pl-6 pt-3 pb-4 rounded-b-4xl h-64">
-            <p className="font-bold text-4xl py-1.5">{title}</p>
+    return <>{(usePromise) && <Link className="w-200 md:w-100 select-none transition hover:scale-103 cursor-pointer group h-max" href={link} target="_blank">
+        <ImageSkeleton width={1600} height={900} alt={`An Image of ${title}`} draggable={false} className="w-full h-50 md:h-30 rounded-t-4xl object-cover transition group-hover:grayscale-90" promise={usePromise}/>
+        <div className="text-white bg-gray-700/60 pl-6 pt-3 pb-4 rounded-b-4xl h-[20rem] md:h-[16rem] flex flex-col items-center md:items-start">
+            <p className="font-bold text-6xl md:text-4xl py-1.5">{title}</p>
             <div className="flex flex-wrap gap-1 py-2">{tags.map((tag)=><Tag title={tag} key={tag}/>)}</div>
-            <p className="font-light text-base">{description}</p>
+            <p className="font-light pt-6 md:pt-2 md:px-0 md:text-base text-3xl px-5">{description}</p>
         </div>
     </Link>}</>
 }
